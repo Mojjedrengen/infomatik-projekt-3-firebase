96f4a3afbb3bf70544964adaa7ef193713ed86c6
Mojjedrengen <99266094+Mojjedrengen@users.noreply.github.com>
2022-04-05 13:14:48 +0200

    Har lavet et canvas og knap

--- sketch.js
+++ sketch.js
@@ -1,5 +1,13 @@
+var database = firebase.database();
+var button;
+
 function setup() {
-  createCanvas(400, 400);
+  var cnv = createCanvas(400, 400);
+  var x = (windowWidth - width) / 2;
+  var y = (windowHeight - height) / 2;
+  cnv.position(x, y);
+  button = createButton("lol");
+  button.position(x, y);
 }
 
 function draw() {
